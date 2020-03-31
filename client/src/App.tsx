4a9485daafad46fa7cd839cf6bf9de7662752430
moboey <60497050+moboey@users.noreply.github.com>
2020-03-31 11:41:40 +0800

    UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,48 @@
 import React, { Component } from 'react'
 import { Link, Route, Router, Switch } from 'react-router-dom'
-import { Grid, Menu, Segment } from 'semantic-ui-react'
+import { Grid, Menu, Segment, Container } from 'semantic-ui-react'
 
-import Auth from './auth/Auth' 
+import Auth from './auth/Auth'
 import { LogIn } from './components/LogIn'
 import { NotFound } from './components/NotFound'
 import { Survey } from './components/Survey'
- 
+
 
 export interface AppProps {
   auth: Auth
   history: any
 }
 
-export interface AppState {}
+export interface AppState { }
 
 export default class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props)
- 
+
   }
- 
+
   render() {
     return (
-      <div>          
-        <Segment style={{ padding: '5em 5em' }}  >
-          <Grid container centered verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={16} centered>
-                <Router history={this.props.history}> 
-                  {this.generateCurrentPage()}
-                </Router>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
+      <div>   <Container style={{ padding: '1em 1em'  }} >
+
+        <Router history={this.props.history}>
+          {this.generateCurrentPage()}
+        </Router>
+      </Container>
       </div>
     )
   }
- 
-  generateCurrentPage() { 
+
+  generateCurrentPage() {
     return (
-      
+
       <Switch>
         <Route
-          path="/"           
+          path="/"
           render={props => {
             return <Survey {...props} />
           }}
-        />         
+        />
       </Switch>
     )
   }
--- client/src/components/Survey.tsx
+++ client/src/components/Survey.tsx
@@ -1,29 +1,17 @@
 import dateFormat from 'dateformat'
-import { History } from 'history'
-import update from 'immutability-helper'
-import * as React from 'react'
-import { PureComponent } from 'react';
+import { History } from 'history' 
+import * as React from 'react' 
 import {
-  PieChart, Pie, Sector, Cell,
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
-
 import {
-  Button,
-  Checkbox,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Input,
-  Image, Label,Statistic,Message,
-  Loader
+  Button,   Header, Icon, 
+  Image, Label, Statistic, Message, Grid, Container, Segment, Loader, Form, Radio
 } from 'semantic-ui-react'
 
-import { createTodo, deleteTodo, getTodos, surveyPost, patchTodo } from '../api/survey-api'
-import Auth from '../auth/Auth'
+import {   getTodos, surveyPost  } from '../api/survey-api' 
 import { SurveyResult } from '../types/Survey'
 
-const COLORS = ['teal', 'orange', 'red'];
 
 
 interface SurveyProps {
@@ -41,7 +29,37 @@ export class Survey extends React.PureComponent<SurveyProps, SurveyState> {
     loadingSurvey: true
   }
 
+  render() {
+    
+    return (
+      <Container >
+        <Header as="h1">Can social distancing flatten the curve?</Header>
+        
+       
+        {this.renderResults()}
+      </Container>
+    )
+  }
 
+  renderResults() {
+
+    if (this.state.loadingSurvey) {
+      return this.renderLoading()
+    }
+    let history = undefined
+    if (this.state.survey.ipAddr) {
+      history = this.renderSurveyHistory()
+    }
+
+    return (
+      <Segment     >
+        {history}
+         {this.renderSurveyInput()}
+        {this.renderChart()}
+        {this.renderSurveyStats()}
+      </Segment>
+      )
+  }
 
   async componentDidMount() {
     try {
@@ -67,171 +85,157 @@ export class Survey extends React.PureComponent<SurveyProps, SurveyState> {
     } catch {
       alert('Post survey failed')
     }
-  }
+  } 
 
-  render() {
-    let history = undefined
-    if(this.state.survey.ipAddr){
-      history = this.renderSurveyHistory()
-    }
-    return (
-      <div>
-        <Header as="h1">Can social distancing flatten the curve?</Header>
+  
 
-        {this.renderSurveyInput()}
-        
-        {this.renderResult()}
-        {history}
 
-      </div>
-    )
-  }
-
-  renderResult() {
+  renderSurveyHistory() {
     if (this.state.loadingSurvey) {
       return this.renderLoading()
     }
-
-    return this.renderChart()
-  }
-
-  renderSurveyHistory() {
+    const COLORS = ["red","yellow","teal"]
     let voted = "No" 
-    if(this.state.survey.vote==='YES_INDEX'){
-      voted="Yes"
-    }else if(this.state.survey.vote==='MAYBE_INDEX'){
-      voted="Not sure"
-    } 
+    if (this.state.survey.vote === 'YES_INDEX') {
+      voted = "Yes" 
+    } else if (this.state.survey.vote === 'MAYBE_INDEX') {
+      voted = "Maybe" 
+    }  
     return (
-      <Grid padded>
-        <Grid.Row><Grid.Column width={16} centered> 
-        <Message size='tiny' >
-    <Message.Header>Welcome back! Changed your mind?</Message.Header>
-    <p>
-      You had last voted '{voted}' on {this.state.survey.when}, using IP address {this.state.survey.ipAddr}. </p>
-      <p> You can change your vote if you wish. </p>
-  </Message>
-          </Grid.Column></Grid.Row>
-          <Grid.Column width={16}>
-            <Divider />
-          </Grid.Column>
-      </Grid>
+      <Segment   textAlign='center' basic  >
+        <Message size='tiny'  >
+        <Message.Header >Welcome back! Changed your mind?</Message.Header>
+        <Message.Content size='tiny'>
+        <p></p><p>
+          You had last voted '{voted}' on {this.state.survey.when}. </p>
+          <p> Your IP address is {this.state.survey.ipAddr}. </p>
+        <p> You can change your vote if you wish. </p></Message.Content>
+      </Message></Segment>
     )
   }
 
+  
   renderSurveyInput() {
     return (
-      <Grid padded>
-        <Grid.Row>
-        <Grid.Column width={16} centered>
-          <Button as='div' labelPosition='right' size='massive'
-          onClick={() => this.onSurveyPost('YES_INDEX')}>
-            <Button color='teal'>
-              <Icon name='checkmark' />Yes
-            </Button>
-            <Label as='a' basic color='teal' pointing='left'>
-            {this.state.survey.yes}
-            </Label>
-          </Button>
-          <Button as='div' labelPosition='right'size='massive'
-          onClick={() => this.onSurveyPost('MAYBE_INDEX')}>
-            <Button color='yellow'>
-              <Icon name='help' />Not sure
-            </Button>
-            <Label as='a' basic color='yellow' pointing='left'>
-            {this.state.survey.maybe}
-            </Label>
-          </Button>
-          <Button as='div' labelPosition='right'size='massive'
-          onClick={() => this.onSurveyPost('NO_INDEX')}>
-            <Button color='red'>
-              <Icon name='ban' />No
-            </Button>
-            <Label as='a' basic color='red' pointing='left'>
-            {this.state.survey.no}
-            </Label>
-          </Button>
-          </Grid.Column> 
-        </Grid.Row>
-      </Grid>
+      <Segment basic vertical style={{display: 'flex',   justifyContent:'center', alignItems:'center' }} > 
+          <Grid  columns='3' padded>
+          <Grid.Column  >
+         
+          <Radio toggle   
+            label='Yes'
+            name='radioGroup'
+            value='YES_INDEX'
+            checked={this.state.survey.vote === 'YES_INDEX'}
+            onChange={() => this.onSurveyPost('YES_INDEX')}
+          />
+         </Grid.Column><Grid.Column> 
+        
+          <Radio toggle
+            label='Maybe'
+            name='radioGroup'
+            value='MAYBE_INDEX'
+            checked={this.state.survey.vote === 'MAYBE_INDEX'}
+            onChange={() => this.onSurveyPost('MAYBE_INDEX')}
+          />
+          </Grid.Column><Grid.Column>
+          <Radio toggle
+            label='No'
+            name='radioGroup'
+            value='NO_INDEX'
+            checked={this.state.survey.vote === 'NO_INDEX'}
+            onChange={() => this.onSurveyPost('NO_INDEX')}
+          />
+        </Grid.Column> </Grid>
+          
+      </Segment>
     )
   }
-
-
-  renderLoading() {
-    return (
-      <Grid.Row>
-        <Loader indeterminate active inline="centered">
-          Loading Survey Result
-        </Loader>
-      </Grid.Row>
-    )
-  }
-
-  renderChart() {
-
-    const data = [
-      { name: 'Yes', value: this.state.survey.yes },
-      { name: 'Not sure', value: this.state.survey.maybe },
-      { name: 'No', value: this.state.survey.no },
-    ];
+  renderSurveyStats() {
+    if (this.state.loadingSurvey) {
+      return this.renderLoading()
+    }
     const totalVotes = this.state.survey.yes + this.state.survey.maybe + this.state.survey.no
     const yesPercent = Math.round((Number(this.state.survey.yes) / Number(totalVotes)) * 100)
     const maybePercent = Math.round((Number(this.state.survey.maybe) / Number(totalVotes)) * 100)
     const noPercent = Math.round((Number(this.state.survey.no) / Number(totalVotes)) * 100)
     return (
+      <Segment   basic   style={{display: 'flex',   justifyContent:'center', alignItems:'center' }} >
+        <Grid  columns='4' padded>
+ <Grid.Column> 
+        <Statistic size='mini'>
+          <Statistic.Value>{yesPercent}%</Statistic.Value>
+          <Statistic.Label>Yes</Statistic.Label>
+        </Statistic>
+        </Grid.Column><Grid.Column>
+        <Statistic size='mini'>
+          <Statistic.Value>{maybePercent}%</Statistic.Value>
+          <Statistic.Label>Maybe</Statistic.Label>
+        </Statistic>
+        </Grid.Column><Grid.Column>
+        <Statistic size='mini'>
+          <Statistic.Value>{noPercent}%</Statistic.Value>
+          <Statistic.Label>No</Statistic.Label>
+        </Statistic>
+        </Grid.Column><Grid.Column>
+        <Statistic size='mini'>
+          <Statistic.Value>{totalVotes}</Statistic.Value>
+          <Statistic.Label>Total</Statistic.Label>
+        </Statistic>
+        </Grid.Column> </Grid>
+      </Segment>
+    )
+  }
+  
 
 
-      <Grid columns='equal'>
-        <Grid.Row>
-          <Grid.Column centered>
-            <PieChart width={390} height={350}>
-              <Pie
-                data={data}
-                cx={210}
-                cy={150}
-                labelLine={false}
-                outerRadius={150}
+  renderLoading() {
+    return (
+      <Segment >
+        <Loader indeterminate active inline="centered">
+          Loading Survey Result
+        </Loader>
+        </Segment>
+    )
+  }
 
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {
-                  data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-                }
-              </Pie>
-            </PieChart></Grid.Column> 
-          <Grid.Column centered>
-            <Grid.Row centered> 
-             
-              
-              <Statistic size='tiny'>
-              <Statistic.Value>{yesPercent}</Statistic.Value>
-              <Statistic.Label>Yes %</Statistic.Label>
-              </Statistic>
+  renderChart() {
 
-              <Statistic size='tiny'>
-              <Statistic.Value>{maybePercent}</Statistic.Value>
-              <Statistic.Label>Not Sure %</Statistic.Label>
-              </Statistic>
+    if (this.state.loadingSurvey) {
+      return this.renderLoading()
+    }
+ 
+    const totalVotes = this.state.survey.yes + this.state.survey.maybe + this.state.survey.no
+    const data = [
+      {
+        name: 'Vote result', Yes: this.state.survey.yes, Maybe: this.state.survey.maybe,  No: this.state.survey.no, amt: totalVotes,
+      },       
+    ];
 
-              <Statistic size='tiny'>
-              <Statistic.Value>{noPercent}</Statistic.Value>
-              <Statistic.Label>No %</Statistic.Label>
-              </Statistic>
-            </Grid.Row> 
-            <Grid.Row centered>
-            <Statistic  centered>
-              <Statistic.Value>{totalVotes}</Statistic.Value>
-              <Statistic.Label>Votes</Statistic.Label>
-              </Statistic>
-            </Grid.Row>
-            </Grid.Column>
-        </Grid.Row> 
-      </Grid>
+    return (
+      <Segment basic style={{display: 'flex',  justifyContent:'center', alignItems:'center' }} >
+      <BarChart
+        width={190}
+        height={190}
+        data={data}
+        margin={{
+          top: 5, right: 0, left: 0, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="Yes" fill="teal" background={{ fill: '#eee' }} />
+        <Bar dataKey="Maybe" fill="orange" />
+        <Bar dataKey="No" fill="red" />
+      </BarChart>
+      </Segment>
     );
 
-  } 
+
+
+  }
 
   calculateDueDate(): string {
     const date = new Date()
