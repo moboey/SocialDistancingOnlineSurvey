10a9f3ee3d4fe8b4c6706845853d2a79bdb262ee
moboey <60497050+moboey@users.noreply.github.com>
2020-03-31 22:42:30 +0800

    final

--- client/src/api/survey-api.ts
+++ client/src/api/survey-api.ts
@@ -1,8 +1,6 @@
 import { apiEndpoint } from '../config'
-import { SurveyResult } from '../types/Survey';
-import { CreateTodoRequest } from '../types/CreateTodoRequest';
-import Axios from 'axios'
-import { UpdateTodoRequest } from '../types/UpdateTodoRequest';
+import { SurveyResult } from '../types/Survey'; 
+import Axios from 'axios' 
 
 export async function getSurvey(): Promise<SurveyResult> {
   console.log('Fetching survey')
@@ -27,60 +25,4 @@ export async function surveyPost(surveyId: string): Promise<SurveyResult> {
   console.log('response from update:'+response.data)
   return response.data.items
 }
-
-export async function createTodo(
-  idToken: string,
-  newTodo: CreateTodoRequest
-): Promise<SurveyResult> {
-  const response = await Axios.post(`${apiEndpoint}/`,  JSON.stringify(newTodo), {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  console.log('response from create:'+response)
-  return response.data.item
-}
-
-export async function patchTodo(
-  idToken: string,
-  todoId: string,
-  updatedTodo: UpdateTodoRequest
-): Promise<void> {
-  await Axios.patch(`${apiEndpoint}/todos/${todoId}`, JSON.stringify(updatedTodo), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${idToken}`
-    }
-  })
-}
-
-export async function deleteTodo(
-  idToken: string,
-  todoId: string
-): Promise<void> {
-  await Axios.delete(`${apiEndpoint}/todos/${todoId}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${idToken}`
-    }
-  })
-}
-
- 
-
-export async function getUploadUrl(
-  idToken: string,
-  todoId: string
-): Promise<string> {
-  const response = await Axios.post(`${apiEndpoint}/todos/${todoId}/attachment`, '', {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${idToken}`
-    }
-  })
-  return response.data.uploadUrl
-}
-
-export async function uploadFile(uploadUrl: string, file: Buffer): Promise<void> {
-  await Axios.put(uploadUrl, file)
-}
+    
